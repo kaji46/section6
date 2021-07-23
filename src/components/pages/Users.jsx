e200f62a35e7499bbca1841d79ecdb4f410766fd
Naoto Kajimura <naotokajimura@gmail.com>
2021-07-23 17:09:35 +0900

    section6 finished

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `ジャケ-${val}`,
+    image: "https://source.unsplash.com/G8cB8hY3yvU/",
+    email: "1111@aaa.com",
+    phone: "080-1111-1111",
+    company: {
+      name: "aaaaかいさy"
+    }
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={users.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
